16d9b0ff5b1ff2bcace7bf6771f49cf16efae736
KevinStock <kevin@kevinstock.net>
2016-08-04 22:08:22 -0500

    added project navigation

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		<link rel="icon" type="image/ico" href="favicon_48x48.ico" />
+	    <link rel="shortcut icon" type="image/ico" href="favicon_48x48.ico" />
+		<title>Thinkful.com FEWD Course</title>
+	</head>
+	<body>
+		<section>
+			<h1>Unit One</h1>
+			<h3>Structure and Style with HTML and CSS</h3>
+			<div id="lesson-one">
+				<a href="lesson-one/">Lesson One: Resume</a>
+				<a href="lesson-two/">Lesson Two: Resume</a>
+				<a href="lesson-three/">Lesson Three: Resume</a>
+				<a href="lesson-four/">Lesson Four: Hack the Times</a>
+				<a href="google-clone/">Lesson Five: Google Clone</a>
+				<a href="small-business-website/">Lesson Six: Small Business Website</a>
+		</section>
+		<section>
+			<h1>Unit Two</h1>
+			<h3>jQuery and GitHub</h3>
+			<div id="unit-two">
+				<a href="jquery-streetfighter/">Lesson 3: Street Fighter</a>
+			</div>
+		</section>
+	<script type="text/javascript">
+  var FHChat = {product_id: "e847a7381081"};
+  FHChat.properties={};FHChat.set=function(key,data){this.properties[key]=data};!function(){var a,b;return b=document.createElement("script"),a=document.getElementsByTagName("script")[0],b.src="https://chat-client-js.firehoseapp.com/chat-min.js",b.async=!0,a.parentNode.insertBefore(b,a)}();
+</script>	
+	</body>
+</html>
